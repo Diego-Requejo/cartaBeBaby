8ccf2a46b9dfb798419f35e7c2949698b0da2380
dierequ <diego.requejo23@estudiantes.uva.es>
2025-12-19 18:44:35 +0100

    no era el final y ahora?

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function openModal(title, text) {
-  document.getElementById('modalTitle').innerText = title;
-  document.getElementById('modalText').innerText = text;
-  document.getElementById('overlay').style.display = 'flex';
-}
-
-function closeModal() {
-  document.getElementById('overlay').style.display = 'none';
-}
